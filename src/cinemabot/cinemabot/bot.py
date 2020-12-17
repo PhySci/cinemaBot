1c903987b9d458afe36cf7b188420107c2ecee37
Fedor <mushenokf@gmail.com>
2020-12-17 21:09:29 +0300

    Working prototype

--- src/cinemabot/cinemabot/bot.py
+++ src/cinemabot/cinemabot/bot.py
@@ -1,9 +1,13 @@
 from cfg import API_KEY
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.utils.callback_data import CallbackData
 import logging
+from pprint import pprint
 
 bot = Bot(token=API_KEY)
 dp = Dispatcher(bot)
+
+date_cb = CallbackData('date_cb', 'action', 'date')
 
 # @dp.message_handler()
 async def echo(message: types.Message):
@@ -21,6 +25,22 @@ async def cmd_start(message: types.Message):
 async def action_cancel(message: types.Message):
     remove_keyboard = types.ReplyKeyboardRemove()
     await message.answer("Действие отменено. Введите /start, чтобы начать заново.", reply_markup=remove_keyboard)
+
+# async def show_calendar():
+
+@dp.message_handler(commands=["show"])
+async def show_calendar(message: types.Message):
+    print(message)
+    calendar_keyboard = types.InlineKeyboardMarkup()
+    calendar_keyboard.row(types.InlineKeyboardButton(text='Today', callback_data=date_cb.new(action='show_date', date='today')),
+                          types.InlineKeyboardButton(text='Tomorrow', callback_data=date_cb.new(action='show_date', date='tomorrow')))
+    await message.answer('Выберите дату', reply_markup=calendar_keyboard)
+
+
+@dp.callback_query_handler(filter())
+async def date_handler(query: types.CallbackQuery):
+    pprint(query.__dict__)
+    await bot.send_message(query.from_user.id, text='Here we are')
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
